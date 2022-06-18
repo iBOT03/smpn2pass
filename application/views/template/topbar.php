ae64b1e0265f1e69e298ce391ccf3c75437f6618
Andrea Santana A <andrea.santana986@gmail.com>
2022-06-18 07:15:43 +0700

    add exiting file

---
+++ application/views/template/topbar.php
@@ -0,0 +1,50 @@
+<header id="header" class="d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+        <h1 class="logo"><a href="index.html">BizLand<span>.</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt=""></a>-->
+
+        <nav id="navbar" class="navbar">
+            <ul>
+                <li><a class="nav-link scrollto active" href="#hero">Beranda</a></li>
+                <li><a class="nav-link scrollto" href="#about">Profil</a></li>
+                <li class="dropdown"><a href="#"><span>Siswa</span> <i class="bi bi-chevron-down"></i></a>
+                    <ul>                        
+                        <li class="dropdown"><a href="#"><span>Siswa</span> <i class="bi bi-chevron-right"></i></a>
+                            <ul>
+                                <li><a href="#">Kelas 7</a></li>
+                                <li><a href="#">Kelas 8</a></li>
+                                <li><a href="#">Kelas 9</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Alumni</a></li>
+                    </ul>
+                </li>
+                <li><a class="nav-link scrollto" href="#services">Service</a></li>
+                <li><a class="nav-link scrollto " href="#portfolio">Berita</a></li>
+                <li><a class="nav-link scrollto" href="#team">PTK</a></li>
+                <!-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+                    <ul>
+                        <li><a href="#">Drop Down 1</a></li>
+                        <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                            <ul>
+                                <li><a href="#">Deep Drop Down 1</a></li>
+                                <li><a href="#">Deep Drop Down 2</a></li>
+                                <li><a href="#">Deep Drop Down 3</a></li>
+                                <li><a href="#">Deep Drop Down 4</a></li>
+                                <li><a href="#">Deep Drop Down 5</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Drop Down 2</a></li>
+                        <li><a href="#">Drop Down 3</a></li>
+                        <li><a href="#">Drop Down 4</a></li>
+                    </ul>
+                </li> -->
+                <li><a class="nav-link scrollto" href="#contact">Kontak</a></li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav><!-- .navbar -->
+
+    </div>
+</header>
